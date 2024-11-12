63a738b3bf93a92f7831512a9eee9cc5b0ef7156
ostapyshyn <ostapyshyn@gmail.com>
2024-11-12 18:49:28 +0200

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <section>
       <h1 className="h1-bold">Home</h1>
+      <h2 className="h1-bold font-space-grotesk">Home</h2>
+      <h3 className="h1-bold font-inter">Home</h3>
     </section>
   );
 }
