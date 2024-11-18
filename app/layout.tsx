33d4a57c854c0b649119074cbf6bdb9a667f63ac
ostapyshyn <ostapyshyn@gmail.com>
2024-11-19 01:05:09 +0200

    Create Auth Routes & Layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,6 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
